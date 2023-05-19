11cd515695268921539f47f35b69e7753ebfd603
ArmandSoto <armand.e.soto@gmail.com>
2023-05-19 17:16:59 -0400

    Goals controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+//middleware that helps organize routing for the endpoint '/api/goals'
+app.use('/api/goals', require('./routes/goalRoutes.js'))
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
